3e66c7cd29f07df406c4ed2dd31ff43329dcf611
Andrea Sessa <andrea.sessa@cleafy.com>
2018-12-03 11:24:25 +0100

    Added day-3 solutions

---
+++ 2018/day-3/index-b.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const fs = require('fs')
+
+const INPUT_FILE = 'input.txt'
+
+const getInput = (filepath) => {
+    return fs.readFileSync(filepath).toString().split('\n')
+}
+
+const parseInput = (rawInput) => {
+    let parsedInput = []
+    getInput(INPUT_FILE).forEach((line) => {
+      if (line != '') {
+        line = line.replace(/\s/g, '')
+        let str1 = line.split('@')
+        let id = str1[0].split('#')[1]
+        let str2 = str1[1].split(':')
+        let location_x = str2[0].split(',')[0]
+        let location_y = str2[0].split(',')[1]
+        let width = str2[1].split('x')[0]
+        let height = str2[1].split('x')[1]
+
+        let rect = {
+          id,
+          location_x: parseInt(location_x),
+          location_y: parseInt(location_y),
+          width: parseInt(width),
+          height: parseInt(height)
+        }
+
+        parsedInput.push(rect)
+      }
+    })
+    return parsedInput
+}
+
+const getAnswer = () => {
+  let grid = Object.create(null);
+  let rects = parseInput()
+
+  rects.forEach((rect) => {
+    for (let i = rect.location_x; i < rect.location_x + rect.width; i++) {
+      for (let j = rect.location_y; j < rect.location_y + rect.height; j++) {
+        grid[`${i},${j}`] = (grid[`${i},${j}`] || 0) + 1;
+      }
+    }
+  })
+
+  rects.forEach((rect) => {
+    let found = false
+    for (let i = rect.location_x; i < rect.location_x + rect.width; i++) {
+      for (let j = rect.location_y; j < rect.location_y + rect.height; j++) {
+          if (grid[`${i},${j}`] !== 1) {
+            found = true
+          }
+      }
+    }
+
+    if (!found) {
+      console.log(rect.id)
+    }
+  })
+}
+
+getAnswer()
