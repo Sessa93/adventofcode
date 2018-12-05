13817408b8344a4816fe2c518ffd858c0ef3febc
Andrea Sessa <andrea.sessa@cleafy.com>
2018-12-05 14:55:41 +0100

    Added day 5 solutions

---
+++ 2018/day-5/index.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const fs = require('fs')
+
+const INPUT_FILE = 'input.txt'
+
+const REACTING_UNITS = [
+  'aA', 'Aa', 'bB', 'Bb', 'cC', 'Cc', 'dD', 'Dd', 'eE', 'Ee', 'fF', 'Ff', 'gG', 'Gg',
+  'hH', 'Hh', 'iI', 'Ii', 'jJ', 'Jj', 'kK', 'Kk', 'lL', 'Ll', 'mM', 'Mm', 'nN', 'Nn',
+  'oO', 'Oo', 'pP', 'Pp', 'qQ', 'Qq', 'rR', 'Rr', 'sS', 'Ss', 'tT', 'Tt', 'uU', 'Uu',
+  'vV', 'Vv', 'wW', 'Ww', 'xX', 'Xx', 'yY', 'Yy', 'zZ', 'Zz',
+];
+
+const getInput = (filepath) => {
+    return fs.readFileSync(filepath).toString().split('\n')
+}
+
+const getAnswer1 = (input) => {
+  let strInput = input[0]
+  let patternFound = false
+
+  while (!patternFound) {
+    patternFound = true
+    REACTING_UNITS.forEach((pair) => {
+      if (strInput.includes(pair)) {
+        strInput = strInput.replace(pair, '');
+        patternFound = false
+      }
+    });
+  }
+  return strInput
+}
+
+const getAnswer2 = (input) => {
+  let patternFound = false
+  let units = {}
+
+  REACTING_UNITS.forEach((pair) => {
+    let tmpStr = input[0]
+    while (!patternFound) {
+      patternFound = true
+      if (tmpStr.includes(pair)) {
+        tmpStr = tmpStr.replace(pair, '')
+        patternFound = false
+      }
+    }
+    units[pair] = tmpStr.length
+  });
+  console.log(units)
+  return Object.entries(units).reduce((a, b) => a[1] < b[1] ? a : b)[1]
+}
+
+console.log('Answer 1: '+getAnswer1(getInput(INPUT_FILE)).length)
+console.log('Answer 2: '+getAnswer2('dabAcCaCBAcCcaDA'))
